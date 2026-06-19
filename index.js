5a71b6035558e42013119840fa6fc75891849819
tahaimage8 <tahaimage8@gmail.com>
2026-06-20 03:03:06 +0600

    Add: Toast & Connect create donation request form

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const app = express()
 const port = 5000
 require('dotenv').config();
+
 app.use(cors());
 app.use(express.json())
 
@@ -12,7 +13,7 @@ app.get('/', (req, res) => {
 })
 
 const uri = process.env.MONGO_DB_URI;
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -20,49 +21,28 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   }
 });
+
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
     const database = client.db("raktanex");
-    const CreateDonationRequestCollection =  database.collection("CreateDonationRequest")
+    const CreateDonationRequestCollection = database.collection("CreateDonationRequest")
 
-
-    app.post('/api/donation-request', async(req,res)=>{
+    app.post('/api/donation-request', async (req, res) => {
       const donations = req.body;
       const result = await CreateDonationRequestCollection.insertOne(donations);
       res.send(result)
     })
 
-
-
-    // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+
+    // await client.close();
   }
 }
+
 run().catch(console.dir);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
